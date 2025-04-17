017b1df01bb2949f39b7901e45e3efe04fdd15cd
gotsysdba <john.lathouwers@oracle.com>
2025-04-17 14:55:12 +0100

    Fix LB NSGs

--- opentofu/nsgs.tf
+++ opentofu/nsgs.tf
@@ -44,32 +44,11 @@ resource "oci_core_network_security_group_security_rule" "server_lb_ingress" {
   }
 }
 
-resource "oci_core_network_security_group_security_rule" "client_lb_egress" {
+resource "oci_core_network_security_group_security_rule" "lb_egress" {
   network_security_group_id = oci_core_network_security_group.lb.id
   description               = "Loadbalancer VCN Access."
   direction                 = "EGRESS"
   protocol                  = "6"
   destination               = module.network.private_subnet_cidr_block
   destination_type          = "CIDR_BLOCK"
-  tcp_options {
-    destination_port_range {
-      min = local.streamlit_client_port
-      max = local.streamlit_client_port
-    }
-  }
-}
-
-resource "oci_core_network_security_group_security_rule" "server_lb_egress" {
-  network_security_group_id = oci_core_network_security_group.lb.id
-  description               = "Loadbalancer VCN Access."
-  direction                 = "EGRESS"
-  protocol                  = "6"
-  destination               = module.network.private_subnet_cidr_block
-  destination_type          = "CIDR_BLOCK"
-  tcp_options {
-    destination_port_range {
-      min = local.fastapi_server_port
-      max = local.fastapi_server_port
-    }
-  }
 }
