bbe909bc9f7ca4d86aa54f9691fbbf98f409b884
corradodebari <corrado.de.bari@oracle.com>
2025-06-10 16:03:12 +0200

    early draft MCP server for RAG

---
+++ src/client/mcp/rag/README.md
@@ -0,0 +1,71 @@
+
++++
+title = 'MCP RAG tool'
+weight = 10
++++
+## Introduction
+This document describe how to re-use the configuration tested in the **AI Optimizer & Toolkit** an expose it as an MCP tool to a local **Claude Desktop**. It will be provided further info to setup as a remote MCP server. This early draft implementation utilizes the `stdio` to interact between the agent dashboard, represented by the **Claude Desktop**, and the tool. 
+
+## Setup
+With **[`uv`](https://docs.astral.sh/uv/getting-started/installation/)** installed, run the following commands in your current project directory `<PROJECT_DIR>/src/client/mcp/rag/`:
+
+```bash
+uv init --python=3.11
+uv venv --python=3.11
+source .venv/bin/activate
+uv add mcp langchain-core==0.3.52 oracledb~=3.1 langchain-community==0.3.21 langchain-huggingface==0.1.2 langchain-openai==0.3.13 langchain-ollama==0.3.2
+```
+
+## Export config
+In the **AI Optimizer & Toolkit** web interface, after tested a configuration, in `Settings/Client Settings`:
+
+![Client Settings](./images/export.png)
+
+* select the checkbox `Include Sensitive Settings` 
+* press button `Download Settings` to download configuration in the project directory: `src/client/mcp/rag` as `optimizer_settings.json`.
+
+## Claude Desktop setup
+
+* In **Claude Desktop** application, in `Settings/Developer/Edit Config`, get the `claude_desktop_config.json` to add the references to the local MCP server for RAG in the `<PROJECT_DIR>/src/client/mcp/rag/`:
+```json
+{
+ "mcpServers": {
+	...
+	,
+	"rag":{
+		"command":"bash",
+		"args":[
+			"-c",
+			"source <PROJECT_DIR>/src/client/mcp/rag/.venv/bin/activate && uv run <PROJECT_DIR>/src/client/mcp/rag/rag_base_optimizer_config_mcp.py"
+		]
+	}
+   }
+}
+```
+* In **Claude Desktop** application, in `Settings/General/Claude Settings/Configure`, under `Profile` tab, update fields like:
+- `Full Name`
+- `What should we call you`
+	
+	and so on, putting in `What personal preferences should Claude consider in responses?`
+	the following text:
+
+```
+#INSTRUCTION:
+Always call the rag_tool tool when the user asks a factual or information-seeking question, even if you think you know the answer.
+Show the rag_tool message as-is, without modification.
+```
+This will impose the usage of `rag_tool` in any case. 
+
+**NOTICE**: If you prefer, in this agent dashboard or any other, you could setup a message in the conversation with the same content of `Instruction` to enforce the LLM to use the rag tool as well.
+
+* Restart **Claude Desktop**.
+
+* You will see two warnings on rag_tool configuration: they will disappear and will not cause any issue in activating the tool.
+
+* Start a conversation. You should see a pop up that ask to allow the `rag` tool usage to answer the questions:
+
+![Rag Tool](./images/rag_tool.png)
+
+ If the question is related to the knowledge base content stored in the vector store, you will have an answer based on that information. Otherwise, it will try to answer considering information on which has been trained the LLM o other tools configured in the same Claude Desktop.
+
+{{% children %}}
---
+++ src/client/mcp/rag/rag_base_optimizer_config_mcp.py
@@ -0,0 +1,210 @@
+from typing import List
+from mcp.server.fastmcp import FastMCP
+import os
+from dotenv import load_dotenv
+#from sentence_transformers import CrossEncoder
+#from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores.utils import DistanceStrategy
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.vectorstores import oraclevs
+from langchain_community.vectorstores.oraclevs import OracleVS
+from langchain_core.documents import BaseDocumentTransformer, Document
+from langchain_community.chat_models.oci_generative_ai import ChatOCIGenAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+import oracledb
+import json
+
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import OllamaLLM
+
+from tqdm import tqdm, trange
+
+print("Successfully imported libraries and modules")
+
+CHUNKS_DIR = "chunks_temp"
+data = {}
+
+# Initialize FastMCP server
+mcp = FastMCP("rag")
+
+
+@mcp.tool()
+def rag_tool(question: str) -> str:
+    """
+    Use this tool to answer any question that may benefit from up-to-date or domain-specific information.
+    
+    Args:
+        question: the question for which are you looking for an answer
+        
+    Returns:
+        JSON string with answer
+    """
+    
+    try: 
+        
+        embeddings = get_embeddings(data)
+        
+        print("Embedding successful!")
+        knowledge_base = get_vectorstore(data,embeddings)
+        print("DB Connection successful!")
+    
+        print("knowledge_base successful!")
+        user_question = question
+        
+        for d in data["prompts_config"]:
+            if d["name"]==data["user_settings"]["prompts"]["sys"]:
+             
+                rag_prompt=d["prompt"]
+       
+        template = """DOCUMENTS: {context} \n"""+rag_prompt+"""\nQuestion: {question} """
+        #template = """Answer the question based only on the following context:{context} Question: {question} """
+        print(template)
+        prompt = PromptTemplate.from_template(template)
+        print("before retriever")
+        print(data["user_settings"]["rag"]["top_k"])
+        retriever = knowledge_base.as_retriever(search_kwargs={"k": data["user_settings"]["rag"]["top_k"]})
+        print("after retriever")
+        
+
+        # Initialize the LLM
+        llm = get_llm(data)
+
+        chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+                | prompt
+                | llm
+                | StrOutputParser()
+        )
+        print("pre-chain successful!")
+        answer = chain.invoke(user_question)
+
+        #print(f"Results provided for question: {question}")
+        #print(f"{answer}")
+    except Exception as e:
+        print(e)
+        print("Connection failed!")
+        answer=""
+
+    return f"{answer}"
+
+def get_llm(data):
+    llm={}
+    llm_config = data["ll_model_config"][data["user_settings"]["ll_model"]["model"]]
+    api=llm_config["api"]
+    url=llm_config["url"]
+    api_key=llm_config["api_key"]
+    model=data["user_settings"]["ll_model"]["model"]
+    print(f"CHAT_MODEL: {model} {api} {url} {api_key}")
+    if api == "ChatOllama":
+        # Initialize the LLM
+        llm = OllamaLLM(
+            model=model,
+            base_url=url
+        )
+    elif api == "OpenAI":
+        
+        llm=llm = ChatOpenAI(
+            model=model,
+            api_key=api_key
+        )
+    return llm
+
+def get_embeddings(data):
+    embeddings={}
+    model=data["user_settings"]["rag"]["model"]
+    api=data["embed_model_config"][model]["api"]
+    url=data["embed_model_config"][model]["url"]
+    api_key=data["embed_model_config"][model]["api_key"]
+    print(f"EMBEDDINGS: {model} {api} {url} {api_key}")
+    embeddings = {}
+    if  api=="OllamaEmbeddings":
+         embeddings=OllamaEmbeddings(
+            model=model,
+            base_url=url)
+    elif api == "OpenAIEmbeddings":
+         print("BEFORE create embbedding")
+         embeddings = OpenAIEmbeddings(
+            model=model,
+            api_key=api_key
+         )   
+         print("AFTER create emebdding")
+    return embeddings
+
+def get_vectorstore(data,embeddings):
+    
+    config=data["database_config"][data["user_settings"]["rag"]["database"]]
+   
+    conn23c = oracledb.connect(user=config["user"], 
+                               password=config["password"], dsn=config["dsn"])
+ 
+    print("DB Connection successful!")
+    metric=data["user_settings"]["rag"]["distance_metric"]
+    
+    dist_strategy=DistanceStrategy.COSINE
+    if metric=="COSINE":
+        dist_strategy=DistanceStrategy.COSINE
+    elif metric == "EUCLIDEAN":
+        dist_strategy=DistanceStrategy.EUCLIDEAN
+   
+    print("1")
+    a=data["user_settings"]["rag"]["vector_store"]
+    print(f"{a}")
+    print(f"BEFORE KNOWLEDGE BASE")
+    print(embeddings)
+    knowledge_base = OracleVS(conn23c, embeddings, data["user_settings"]["rag"]["vector_store"], dist_strategy)
+    return knowledge_base
+
+def get_conf(data):
+
+    # Explore top-level keys
+    print("Top-level keys:", list(data.keys()))
+
+    # Example: Access user_settings
+    user_settings = data.get("user_settings", {})
+    print("\nUser Settings Keys:", list(user_settings.keys()))
+
+    # Drill down to ll_model
+    ll_model = user_settings.get("ll_model", {})
+    print("\nLL Model Settings:")
+    for key, value in ll_model.items():
+        print(f"  {key}: {value}")
+
+    llm_config = data["ll_model_config"][user_settings["ll_model"]["model"]]
+    for key, value in llm_config.items():
+        print(f"  {key}: {value}")
+  
+    #llmodel type to import the right dir:
+    llm_config = data["ll_model_config"][user_settings["ll_model"]["model"]]
+    api=llm_config["api"]
+    url=llm_config["url"]
+    api_key=llm_config["api_key"]
+    
+
+
+if __name__ == "__main__":
+    rag_tool_desc=[
+    f"""
+    Use this tool to answer any question that may benefit from up-to-date or domain-specific information.
+    
+    Args:
+        question: the question for which are you looking for an answer
+        
+    Returns:
+        JSON string with answer
+    """
+    ]
+
+
+    # Initialize and run the server
+    # Load JSON file
+    file="optimizer_settings.json"
+    with open(file, "r") as file:
+        #rag_tool.__doc__=rag_tool_desc[0]
+        data = json.load(file)
+        mcp.run(transport='stdio')
