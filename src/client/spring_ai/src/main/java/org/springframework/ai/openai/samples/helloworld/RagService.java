97b09174f7b837b49ffc29f5863b6ec8aa44f190
corradodebari <corrado.de.bari@oracle.com>
2025-06-30 10:53:14 +0200

    license added

--- src/client/spring_ai/src/main/java/org/springframework/ai/openai/samples/helloworld/RagService.java
+++ src/client/spring_ai/src/main/java/org/springframework/ai/openai/samples/helloworld/RagService.java
@@ -1,3 +1,7 @@
+/*
+Copyright (c) 2024, 2025, Oracle and/or its affiliates.
+Licensed under the Universal Permissive License v1.0 as shown at http://oss.oracle.com/licenses/upl.
+*/
 package org.springframework.ai.openai.samples.helloworld;
 
 import org.springframework.stereotype.Service;
