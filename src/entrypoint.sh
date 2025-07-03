f32ef7b3783de07f3b2011f08faab77b956aaa86
gotsysdba <john.lathouwers@oracle.com>
2025-07-03 10:48:26 +0100

    Fixes #203

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,6 +3,14 @@
 ## Licensed under the Universal Permissive License v1.0 as shown at http://oss.oracle.com/licenses/upl.
 # spell-checker:ignore streamlit
 
+OCI_DIR="/app/.oci"
+CONFIG_FILE="${OCI_DIR}/config"
+
+# Process the OCI config file to change the path of the key_file
+KEY_FILE_PATH=$(grep '^key_file=' "$CONFIG_FILE" | cut -d'=' -f2)
+KEY_FILE_NAME=$(basename "$KEY_FILE_PATH")
+sed -i.bak "s|key_file=.*|key_file=${OCI_DIR}/${KEY_FILE_NAME}|g" "$CONFIG_FILE"
+
 if [ -d /app/server ] && [ -d /app/client ]; then
     echo "Starting Application (Client and Server)"
     exec streamlit run ./launch_client.py
